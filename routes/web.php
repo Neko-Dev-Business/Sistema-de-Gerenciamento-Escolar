5975173ceef5a140f5c729ea192e9637a1097297
Gabriel Oliveira <analista.gabrieloliveira@gmail.com>
2023-09-25 00:26:17 -0300

    #FIX LoginController + #CSS

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\PessoaController;
+use App\Http\Controllers\ContratadoController;
+use App\Http\Controllers\AssociadoController;
+use App\Http\Controllers\VendaController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,15 +17,10 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 //ROTA DE LOGIN
 Route::get('/', [LoginController::class, 'index'])->name('login.index');
