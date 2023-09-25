24c8470c9937809b5089f359f43d12b0265de8ee
Gabriel Oliveira <analista.gabrieloliveira@gmail.com>
2023-09-24 23:28:30 -0300

    #View Login

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="shortcut icon" href="/images/layout/icon_arpa.png" type="image/x-icon">
+    <link rel="stylesheet" href="/css/icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="/css/template.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    </head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark mb-4 template-bg">
+        <div class="container">
+          <a href="{{ route('dashboard.index') }}"><img src="/images/layout/logo_arpanet_color.png" height="30" alt="ARPAnet"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item px-3">
+                <a class="nav-link text-white" href="{{route('dashboard.index')}}"><i id="main" class="bi bi-house-door color-white"></i>Home</a>
+              </li>
+              <li class="nav-item px-3 dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" ><i id="main" class="bi bi-person"></i>Cadastros</a>
+                <ul class="dropdown-menu">
+                    {{-- <li><a href="{{route('pessoas.create')}}" class="dropdown-item">Cadastrar Novo</a></li>
+                    <option value="asssociado">-----------------------</option> --}}
+                    <li><a href="{{route('pessoas.index')}}" class="dropdown-item">Pessoas</a></li>
+                    <li><a href="{{ route('associados.index') }}" class="dropdown-item">Associados</a></li>
+                    <li><a href="{{ route('contratados.index') }}" class="dropdown-item">Contratados</a></li>
+                </ul>
+              </li>
+              <li class="nav-item px-3">
+                {{--Produtos não salvam no banco :D--}}
+                <a class="nav-link text-white" href="{{route('produtos.index')}}"><i id="main" class="bi bi-box-seam color-white"></i>Produtos</a> {{-- Rever a estrutura dos produtos --}}
+              </li>
+              <li class="nav-item px-3">
+                {{--Vendas Incompleto, faltam as ligações com o banco--}}
+                <a class="nav-link text-white" href="{{route('vendas.index')}}"><i id="main" class="bi bi-cash-coin color-white"></i>Vendas</a>
+              </li>
+
+              @can('acessar-usuarios')
+                <li class="nav-item px-3">
+                  <a class="nav-link text-white" href="{{route('usuarios.index')}}"><i id="main" class="bi bi-key color-white"></i>Usuários</a>
+                </li>
+              @endcan
+              <li class="nav-item px-3 dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"><img class="template-user" src="/storage/usuarios/{{ auth()->user()->foto}}" alt="{{ auth()->user()->name }}" />{{ auth()->user()->name }}</a>
+
+                <ul class="dropdown-menu dropdown-menu-right">
+                    <li><a href="{{ route('usuarios.alt', auth()->user()->id) }}" class="dropdown-item" title="Alterar Dados, Senha, Foto de Perfil"><i class="bi bi-gear"></i> Configurações</a></li>
+                    <li><a href="#" class="dropdown-item" title="Ajuda e Suporte"><i class="bi bi-question-circle"></i> Ajuda e Suporte</a></li>
+                    <li><a href="{{ route('login.logout') }}" class="dropdown-item" title="Sair da aplicação"><i class="bi bi-box-arrow-in-right"></i> Sair</a></li>
+                </ul>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div class="container mb-3 p-4 bg-white shadow h-100 position-relative">
+        @yield('conteudo')
+
+      </div>
+      <div class="template-footer-fixed">
+        <footer class="sticky-footer container-fluid p-3 text-center template-bg mt-5 text-white text-lg-start mx-auto">
+            <div class="container text-center text-md-start d-flex flex-row justify-content-center align-items-start">
+              <div class="mx-auto flex-fill align-self-center">
+                <a href="{{ route('dashboard.index') }}"><img src="/images/layout/logo_branca.png" alt="ARPAnet" height="50px" /></a>
+              </div>
+              <div class="mx-auto flex-fill">
+                <h6 class="fw-bold mb-2">Início</h6>
+                <span class="d-block"><a href="{{ route('dashboard.index') }}" class="text-reset text-decoration-none">Home</a></span>
+                <span class="d-block"><a href="#" class="text-reset text-decoration-none">Associados</a></span>
+                {{-- <span class="d-block"><a href="#" class="text-reset text-decoration-none">Contratados</a></span>
+                <span class="d-block"><a href="#" class="text-reset text-decoration-none">Compradores</a></span> --}}
+              </div>
+              <div class="mx-auto flex-fill">
+                <h6 class="fw-bold mb-2">Sobre nós</h6>
+                <span class="d-block"><a href="https://www.blogger.com/profile/14072350492570167936"  target="_blank" class="text-reset text-decoration-none">Informações</a></span>
+                <span class="d-block"><a href="http://arpareciclagem.blogspot.com"  target="_blank" class="text-reset text-decoration-none">Blog</a></span>
+              </div>
+              <div class="mx-auto flex-fill">
+                <h6 class="fw-bold mb-2">Redes</h6>
+                  <div>
+                    <a href="https://pt-br.facebook.com/reciclagemarpa/" target="_blank" class="me-3 text-reset" title="Facebook"><i class="bi bi-facebook template-txt"></i></a>
+                    <a href="https://www.instagram.com/arpa7395/"  target="_blank" class="me-3 text-reset" title="Instagram"><i class="bi bi-instagram template-txt"></i></a>
+                  </div>
+              </div>
+            </div>
+        </footer>
+        <div class="text-center p-2 template-footer text-white">
+          <span class="text-white-50">&copy; Copyright -</span> ARPAnet <span class="text-white-50">by</span> Centro Universitário UniRios.
+          Todos os direitos reservados.
+        </div>
+      </div>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/template.js"></script>
+    {{-- <script scr="{{ asset('js/template.js') }}"></script> --}}
+</body>
+</html>
---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ARPAnet</title>
+    <link rel="icon" href="/images/layout/icon_arpa.png">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/icons/bootstrap-icons.css ">
+    <link rel="stylesheet" href="/css/template.css">
+</head>
+<body id="login"  class="bg-primary">
+    {{-- <div class="mask d-flex align-items-center h-100" style="background-color: rgba(0, 0, 0, 0.8); height: 100% !important; margin: 0; padding: 0;"> --}}
+        <div class="col-xl-3 bg-white p-5 rounded-3 shadow position-absolute top-50 start-50 translate-middle">
+            <img src="/images/layout/logo_arpanet.png" alt="ARPAnet" height="70" class="d-block mx-auto mb-4">
+
+            @if (Session::get('erro'))
+            <div class="alert alert-danger text-center p-2">{{ Session::get('erro') }}</div>
+            @endif
+
+            @if ($errors->any())
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-warning text-center p-2">{{ $error }}</div>
+                @endforeach
+            @endif
+
+            <form class="row g-4 mb-3" action="{{ route('login.auth') }}" method="POST">
+                @csrf
+                <div class="col-12">
+                    <label for="email" class="form-label fs-5 fs-5">E-mail</label>
+                    <input type="email" class="bg-light form-control form-control-lg" id="email" name="email">
+                </div>
+                <div class="col-12">
+                    <label for="password" class="form-label fs-5 fs-5">Senha</label>
+                    <input type="password" class="bg-light form-control form-control-lg" id="password" name="password">
+                </div>
+                <div class="col-12 d-grid">
+                    <button type="submit" class="template-btn btn btn-lg btn-primary">Entrar</button>
+                </div>
+            </form>
+            <div class="text-center fixed-bottom mb-2 template-fnt-alt" style="font-size: 1em;">
+                &copy; Copyright - ARPAnet by <br /> Centro Universitário UniRios.
+            </div>
+        </div>
+    {{-- </div> --}}
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+{{-- BY: DIEGO ALBUQUERQUE DE CARVALHO --}}
