1b52b72be784002d87f90031b95b2ff6cfab0e1b
Gabriel Oliveira <analista.gabrieloliveira@gmail.com>
2023-09-25 00:20:58 -0300

    #Controller LoginCrontroller - Erro 404

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,31 @@ Route::get('/', function () {
 Route::get('/', [LoginController::class, 'index'])->name('login.index');
 Route::post('login/auth', [LoginController::class, 'auth'])->name('login.auth');
 Route::get('login/logout', [LoginController::class, 'logout'])->name('login.logout');
+
+//ROTAS USUARIOS
+//INDEX
+Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios.index');
+
+//CRIAR USUÁRIO
+Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios.create');
+Route::post('/usuarios', [UsuarioController::class, 'store'])->name('usuarios.store');
+
+//DELETAR USUÁRIO
+Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy'])->name('usuarios.destroy'); //deletar registro
+
+//EDITAR USUÁRIO
+Route::get('/usuarios/edit/{id}', [UsuarioController::class, 'edit'])->name('usuarios.edit'); //formulário de edição
+Route::put('/usuarios/{id}', [UsuarioController::class, 'update'])->name('usuarios.update'); //atualizar registro
+
+//EDITAR USUÁRIO LOGADO
+Route::get('/usuarios/alterar/{id}', [UsuarioController::class, 'alt'])->name('usuarios.alt'); //formulário de atualização
+// Route::get('/usuarios/alterar/{id}/{modal?}', [UsuarioController::class, 'alt'])->name('usuarios.alt'); //formulário de atualização
+Route::put('/usuarios/atualizar/{id}', [UsuarioController::class, 'update_alt'])->name('usuarios.update_alt'); //atualizar registro
+
+//TROCAR SENHA USUÁRIO LOGADO
+// Route::get('/usuarios/pass/{id}', [UsuarioController::class, 'pass'])->name('usuarios.pass');
+Route::put('/usuarios/updatepass/{id}', [UsuarioController::class, 'updatepass'])->name('usuarios.updatepass'); //trocar senha
+/* CEMITÉRIO ✞ */
+// Route::put('/usuarios/modifypass/{id}', [UsuarioController::class, 'modifypass'])->name('usuarios.modifypass'); //trocar senha
+// function ($id) {return 'User '.$id;} , -> TESTE VARIÁVEL
+// array_merge(\Illuminate\Support\Facades\Route::current()->parameters(), ['locale' => 'en'])
