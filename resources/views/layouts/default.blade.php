706c43a94fd572a2b0d9a7e9c6672dc8508f03ce
Daniel Marques <dmarques2000@gmail.com>
2023-09-25 03:36:54 -0300

    #Menu novo menu

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,44 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="shortcut icon" href="/images/layout/icon_arpa.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/imagens/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="/css/icons/bootstrap-icons.css">
     <link rel="stylesheet" href="/css/template.css">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/custom.css">
     </head>
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark mb-4 template-bg">
-        <div class="container">
-          <a href="{{ route('dashboard.index') }}"><img src="/images/layout/logo_arpanet_color.png" height="30" alt="ARPAnet"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item px-3">
-                <a class="nav-link text-white" href="{{route('dashboard.index')}}"><i id="main" class="bi bi-house-door color-white"></i>Home</a>
-              </li>
-              <li class="nav-item px-3 dropdown">
-                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" ><i id="main" class="bi bi-person"></i>Cadastros</a>
-                <ul class="dropdown-menu">
-                    {{-- <li><a href="{{route('pessoas.create')}}" class="dropdown-item">Cadastrar Novo</a></li>
-                    <option value="asssociado">-----------------------</option> --}}
-                    <li><a href="#" class="dropdown-item">Pessoas</a></li>
-                    <li><a href="#" class="dropdown-item">Associados</a></li>
-                    <li><a href="#" class="dropdown-item">Contratados</a></li>
-                </ul>
-              </li>
-              <li class="nav-item px-3">
-                {{--Produtos não salvam no banco :D--}}
-                <a class="nav-link text-white" href="#"><i id="main" class="bi bi-box-seam color-white"></i>Produtos</a> {{-- Rever a estrutura dos produtos --}}
-              </li>
-              <li class="nav-item px-3">
-                {{--Vendas Incompleto, faltam as ligações com o banco--}}
-                <a class="nav-link text-white" href="#"><i id="main" class="bi bi-cash-coin color-white"></i>Vendas</a>
-              </li>
 
-              @can('acessar-usuarios')
+    <!-- Sidebar -->
+    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+      <div class="position-sticky">
+
+        <div class="list-group list-group-flush mx-3 mt-4">
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+            <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Dashboard</span>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple ">
+            <i class="fas fa-chart-area fa-fw me-3"></i><span>Minhas Notas</span>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
+              class="fas fa-lock fa-fw me-3"></i><span>Avaliações</span></a>
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
+              class="fas fa-chart-line fa-fw me-3"></i><span>Horário</span></a>
+
+        </div>
+      </div>
+    </nav>
+    <!-- Sidebar -->
+
+    <!-- Navbar -->
+    <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+      <!-- Container wrapper -->
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu"
+          aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fas fa-bars"></i>
+
+        </button>
+        <a class="navbar-brand " href="{{ route('dashboard.index') }}">
+          <img src="imagens/logo.png" height="50" alt="SysEdu"
+            loading="lazy" />
+        </a>
+
+          @can('acessar-usuarios')
                 <li class="nav-item px-3">
                   <a class="nav-link text-white" href="#"><i id="main" class="bi bi-key color-white"></i>Usuários</a>
                 </li>
@@ -55,48 +62,22 @@
                     <li><a href="#" class="dropdown-item" title="Ajuda e Suporte"><i class="bi bi-question-circle"></i> Ajuda e Suporte</a></li>
                     <li><a href="#" class="dropdown-item" title="Sair da aplicação"><i class="bi bi-box-arrow-in-right"></i> Sair</a></li>
                 </ul>
-              </li>
 
-            </ul>
-          </div>
-        </div>
-      </nav>
+          </li>
+        </ul>
+      </div>
+      <!-- Container wrapper -->
+    </nav>
+  <main style="margin-top: 58px;">
+    <div class="container pt-4"></div>
+  </main>
+  <!--Main layout-->
 
       <div class="container mb-3 p-4 bg-white shadow h-100 position-relative">
         @yield('conteudo')
 
       </div>
-      <div class="template-footer-fixed">
-        <footer class="sticky-footer container-fluid p-3 text-center template-bg mt-5 text-white text-lg-start mx-auto">
-            <div class="container text-center text-md-start d-flex flex-row justify-content-center align-items-start">
-              <div class="mx-auto flex-fill align-self-center">
-                <a href="{{ route('dashboard.index') }}"><img src="/images/layout/logo_branca.png" alt="ARPAnet" height="50px" /></a>
-              </div>
-              <div class="mx-auto flex-fill">
-                <h6 class="fw-bold mb-2">Início</h6>
-                <span class="d-block"><a href="{{ route('dashboard.index') }}" class="text-reset text-decoration-none">Home</a></span>
-                <span class="d-block"><a href="#" class="text-reset text-decoration-none">Associados</a></span>
-                {{-- <span class="d-block"><a href="#" class="text-reset text-decoration-none">Contratados</a></span>
-                <span class="d-block"><a href="#" class="text-reset text-decoration-none">Compradores</a></span> --}}
-              </div>
-              <div class="mx-auto flex-fill">
-                <h6 class="fw-bold mb-2">Sobre nós</h6>
-                <span class="d-block"><a href="https://www.blogger.com/profile/14072350492570167936"  target="_blank" class="text-reset text-decoration-none">Informações</a></span>
-                <span class="d-block"><a href="http://arpareciclagem.blogspot.com"  target="_blank" class="text-reset text-decoration-none">Blog</a></span>
-              </div>
-              <div class="mx-auto flex-fill">
-                <h6 class="fw-bold mb-2">Redes</h6>
-                  <div>
-                    <a href="https://pt-br.facebook.com/reciclagemarpa/" target="_blank" class="me-3 text-reset" title="Facebook"><i class="bi bi-facebook template-txt"></i></a>
-                    <a href="https://www.instagram.com/arpa7395/"  target="_blank" class="me-3 text-reset" title="Instagram"><i class="bi bi-instagram template-txt"></i></a>
-                  </div>
-              </div>
-            </div>
-        </footer>
-        <div class="text-center p-2 template-footer text-white">
-          <span class="text-white-50">&copy; Copyright -</span> ARPAnet <span class="text-white-50">by</span> Centro Universitário UniRios.
-          Todos os direitos reservados.
-        </div>
+
       </div>
     <script src="/js/bootstrap.bundle.min.js"></script>
     <script src="/js/template.js"></script>
