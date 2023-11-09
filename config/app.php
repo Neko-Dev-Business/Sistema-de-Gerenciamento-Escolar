ce4db58ced8b8157232a09fcee30e2ab8f049165
Daniel Marques da Silva <airbokl41@gmail.com>
2023-11-09 10:43:20 -0300

    #php - configs

--- config/app.php
+++ config/app.php
@@ -208,12 +208,13 @@ return [
     |
     */
 
+    /*
     'providers' => [ 
         Barryvdh\DomPDF\ServiceProvider::class,
       ],
       'aliases' => [
        'PDF' => Barryvdh\DomPDF\Facade::class,
       ]
-
+*/
 ];
 
